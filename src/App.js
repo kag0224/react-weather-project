c88a355c03795afb10c17cbed66de5ff04b42d38
kag0224 <101907323+kag0224@users.noreply.github.com>
2022-07-29 16:17:03 -0700

    added styling

--- src/App.js
+++ src/App.js
@@ -6,17 +6,15 @@ export default function App() {
   return (
     <div className="App">
       <div className="container">
-        <h1>Weather App</h1>
         <Weather />
         <footer>
-          This project was coded by Kristy-Ann Gaor and is{" "}
+          This project was coded by Kristy-Ann Gaor and is open-sourced on{" "}
           <a
             href="https://github.com/kag0224/react-weather-project"
             target="_blank"
             rel="noopener noreferrer"
           >
-            {" "}
-            open-sourced on GitHub
+            GitHub
           </a>
         </footer>
       </div>
--- src/Weather.js
+++ src/Weather.js
@@ -11,10 +11,15 @@ export default function Weather() {
               type="search"
               placeholder="Enter a city"
               className="form-control"
+              autoFocus="on"
             />
           </div>
           <div className="col-3">
-            <input type="submit" value="Search" className="btn btn-primary" />
+            <input
+              type="submit"
+              value="Search"
+              className="btn btn-primary w-100"
+            />
           </div>
         </div>
       </form>
@@ -23,13 +28,17 @@ export default function Weather() {
         <li>Wednesday 07:00</li>
         <li>Mostly cloudy</li>
       </ul>
-      <div className="row">
+      <div className="row mt-4">
         <div className="col-6">
-          <img
-            src="http://openweathermap.org/img/wn/02n@2x.png"
-            alt="mostly cloudy"
-          />
-          6°C
+          <div className="clearfix">
+            <img
+              src="http://openweathermap.org/img/wn/02n@2x.png"
+              alt="mostly cloudy"
+              className="float-left"
+            />
+            <span className="temperature">6</span>
+            <span className="unit">°C</span>
+          </div>
         </div>
         <div className="col-6">
           <ul>
