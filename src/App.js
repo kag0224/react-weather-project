04752fb7fc083975adce804db8c183f51814629e
kag0224 <101907323+kag0224@users.noreply.github.com>
2022-07-29 14:03:08 -0700

    built weather app structure

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
 import "./App.css";
+import React from "react";
+import Weather from "./Weather";
 
 export default function App() {
   return (
     <div className="App">
-      <h1>Hi from Weather</h1>
+      <div className="container">
+        <h1>Weather App</h1>
+        <Weather />
+        <footer>
+          This project was coded by Kristy-Ann Gaor and is{" "}
+          <a
+            href="https://github.com/kag0224/react-weather-project"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            {" "}
+            open-sourced on GitHub
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
