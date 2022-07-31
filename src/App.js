7be67b879038e99ea2f2c479e57cbf8438c79f92
kag0224 <101907323+kag0224@users.noreply.github.com>
2022-07-31 14:17:50 -0700

    linked netlify

--- src/App.js
+++ src/App.js
@@ -15,6 +15,15 @@ export default function App() {
             rel="noopener noreferrer"
           >
             GitHub
+          </a>{" "}
+          and hosted on{" "}
+          <a
+            href="https://app.netlify.com/sites/celadon-gingersnap-578713/overview"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            {" "}
+            Netlify{" "}
           </a>
         </footer>
       </div>
